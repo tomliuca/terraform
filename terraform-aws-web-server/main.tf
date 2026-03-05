0a473fcd826618ea99a58aa2d33f86aacb0ae9fd
abecomputersinc <abecomputersinc@gmail.com>
2026-03-05 14:51:28 -0500

    Update main.tf

--- terraform-aws-web-server/main.tf
+++ terraform-aws-web-server/main.tf
@@ -199,7 +199,7 @@ resource "aws_instance" "web" {
   # orphaned EBS volumes accumulating on your account.
   root_block_device {
     volume_type           = "gp3"
-    volume_size           = 20
+    volume_size           = 30
     delete_on_termination = true
 
     tags = {
